454d0623095c26bc8e627879c2bcef538e3d545e
V-Marko <markvardanian794@gmail.com>
2025-09-13 18:25:24 +0400

    frst commit

---
+++ app/src/main/java/com/example/plantdiseases/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.plantdiseases;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+
+    }
+
+
+}
